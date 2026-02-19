74abccd77ddc3f53e42f6acf138144b6e1c0edd6
mandistep2026 <mandistep2026@outlook.com>
2026-02-19 15:19:59 +0000

    28

--- core/__init__.py
+++ core/__init__.py
@@ -2,8 +2,11 @@
 from .kernel import Kernel, Process, ProcessState, MemoryManager, Scheduler
 from .filesystem import FileSystem, Inode, FileType
 from .persistence import PersistenceManager
+from .user import User, UserManager, Group
+from .auth import Session, Authenticator
 
 __all__ = [
     'Kernel', 'Process', 'ProcessState', 'MemoryManager', 'Scheduler',
-    'FileSystem', 'Inode', 'FileType', 'PersistenceManager'
+    'FileSystem', 'Inode', 'FileType', 'PersistenceManager',
+    'User', 'UserManager', 'Group', 'Session', 'Authenticator'
 ]
