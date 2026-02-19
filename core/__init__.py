325d3aed5e9eb385c46732345a54973b39ae8d1d
mandistep2026 <mandistep2026@outlook.com>
2026-02-19 14:56:19 +0000

    10

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,9 @@
 """PureOS Core Module"""
 from .kernel import Kernel, Process, ProcessState, MemoryManager, Scheduler
 from .filesystem import FileSystem, Inode, FileType
+from .persistence import PersistenceManager
 
 __all__ = [
     'Kernel', 'Process', 'ProcessState', 'MemoryManager', 'Scheduler',
-    'FileSystem', 'Inode', 'FileType'
+    'FileSystem', 'Inode', 'FileType', 'PersistenceManager'
 ]
